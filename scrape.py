a4dd1e877e8bcbcc850edab5d81655ec46b86036
Simon Bächler <simon@stellanera.com>
2018-02-28 00:00:23 +0100

    Add Mongodb export

--- scrape.py
+++ scrape.py
@@ -5,6 +5,14 @@ import tmdbsimple as tmdb
 from dotenv import load_dotenv, find_dotenv
 import time
 
+def delete_folder(pth):
+    for sub in pth.iterdir():
+        if sub.is_dir():
+            delete_folder(sub)
+        else:
+            sub.unlink()
+    pth.rmdir()
+
 load_dotenv(find_dotenv())
 tmdb.API_KEY = os.environ.get('TMDB_KEY')
 
@@ -14,6 +22,7 @@ discover = tmdb.Discover()
 popular_movies = discover.movie(sort_by='popularity.desc')
 
 movies_path = pathlib.Path('./data/movies')
+delete_folder(movies_path)
 movies_path.mkdir(parents=True, exist_ok=True)
 
 (movies_path / 'GET.json').write_text(json.dumps(popular_movies))
@@ -28,6 +37,9 @@ genres_path.mkdir(parents=True, exist_ok=True)
 
 (genres_path / 'GET.json').write_text(json.dumps(all_genres))
 
+mongo_genres = {}
+mongo_movies = {}
+mongo_reviews = {}
 
 for result in popular_movies['results']:
     if (result['id']):
@@ -45,6 +57,18 @@ for result in popular_movies['results']:
             'reviews': reviews['results'],
         }
 
+        mongo_movies[movie.id] = info
+        mongo_reviews[movie.id] = reviews
+        for genre in info['genres']:
+            mongo_genres.update(genre)
+
         (detail_path / 'GET.json').write_text(json.dumps(details))
 
     time.sleep(1)  # second
+
+# Write the Mongodb dump
+mongo_path = pathlib.Path('./mongo-dump')
+mongo_path.mkdir(parents=True, exist_ok=True)
+(mongo_path / 'movies.json').write_text(json.dumps(list(mongo_movies.values())))
+(mongo_path / 'genres.json').write_text(json.dumps(all_genres))
+(mongo_path / 'reviews.json').write_text(json.dumps(list(mongo_reviews.values())))
